f60e233c3e8d3e0621b870b58cb9183c3e06069d
zharif <zharif@ig-interactive.com>
2018-03-30 22:49:16 +0800

    Initial Commit

---
+++ BannerAds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BannerAds
+//
+//  Created by M. Zharif Hadi M. Khairuddin on 30/03/2018.
+//  Copyright © 2018 M. Zharif Hadi M. Khairuddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
